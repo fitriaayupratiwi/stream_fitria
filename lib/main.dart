829c852cb6c0a43975b6da275fba5edd75af1966
fitriaayupratiwi <fitriaayupratwi@gmail.com>
2024-12-18 08:24:47 -0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Stream_Fitria',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: const StreamHomepage(),
+    );
+  }
+}
+
+class StreamHomepage extends StatefulWidget {
+  const StreamHomepage({super.key});
+
+  @override
+  State<StreamHomepage> createState() => _StreamHomepageState();
+}
+
+class _StreamHomepageState extends State<StreamHomepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
