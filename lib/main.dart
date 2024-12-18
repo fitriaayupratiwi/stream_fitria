a7c1f079196b2416325a46a99d5c5e322c91b23b
fitriaayupratiwi <fitriaayupratwi@gmail.com>
2024-12-18 08:25:13 -0800

    W13: Jawaban Soal 1

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Stream_Fitria',
+      title: 'Stream_Fitri',
       theme: ThemeData(
         primarySwatch: Colors.purple,
       ),
